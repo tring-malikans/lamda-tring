ce01246aee99d6f7b3a5c49cf3b498d8c656b209
sufiyank97 <sufiyan7697@gmail.com>
2021-09-21 20:52:21 +0530

    added

--- app/main.py
+++ app/main.py
@@ -110,4 +110,4 @@ handler = Mangum(app=app)
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host="0.0.0.0", port=9000, reload=True)
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
